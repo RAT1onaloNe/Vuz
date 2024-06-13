d58e4790a47aa141eafc28d769a8ce0937b9bd89
RAT1onaloNe <127165209+RAT1onaloNe@users.noreply.github.com>
2024-06-13 19:18:00 +0300

    Add files via upload

---
+++ ПР4_итератор.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+
+class Iterator {
+public:
+    Iterator(const std::vector<int>& container)
+        : _container(container), _index(0) {}
+
+    void next() {
+        ++_index;
+    }
+
+    int value() const {
+        return _container[_index];
+    }
+
+    bool hasMore() const {
+        return _index < _container.size();
+    }
+
+private:
+    const std::vector<int>& _container;
+    size_t _index;
+};
+
+int main() {
+    std::vector<int> vec = {1, 3, 32, 2};
+    Iterator it(vec);
+
+    while (it.hasMore()) {
+        std::cout << it.value() << " ";
+        it.next();
+    }
+
+    return 0;
+}
---
+++ ПР4_посетитель.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <vector>
+
+class Visitor;
+
+class Item {
+public:
+    virtual void accept(Visitor& visitor) = 0;
+    virtual ~Item() = default;
+};
+
+class Visitor {
+public:
+    virtual void visitItem1(class Item1* item) = 0;
+    virtual void visitItem2(class Item2* item) = 0;
+    virtual ~Visitor() = default;
+};
+
+class Item1 : public Item {
+public:
+    void accept(Visitor& visitor) override {
+        visitor.visitItem1(this);
+    }
+    void specificOperation1() {
+        std::cout << "Item 1" << std::endl;
+    }
+};
+
+class Item2 : public Item {
+public:
+    void accept(Visitor& visitor) override {
+        visitor.visitItem2(this);
+    }
+    void specificOperation2() {
+        std::cout << "Item 2" << std::endl;
+    }
+};
+
+class ConcreteVisitor : public Visitor {
+public:
+    void visitItem1(Item1* item) override {
+        item->specificOperation1();
+    }
+    void visitItem2(Item2* item) override {
+        item->specificOperation2();
+    }
+};
+
+int main() {
+    Item1 item1;
+    Item2 item2;
+    ConcreteVisitor visitor;
+
+    item1.accept(visitor);
+    item2.accept(visitor);
+
+    return 0;
+}
