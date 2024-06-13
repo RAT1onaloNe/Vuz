113a6bc4049b63e8cf26bec942983160094bc265
RAT1onaloNe <127165209+RAT1onaloNe@users.noreply.github.com>
2024-06-13 18:34:50 +0300

    Add files via upload

---
+++ ПР3_шаблонный_метод.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+// Абстрактный базовый класс, определяющий скелет алгоритма
+class AbstractClass {
+public:
+    // Шаблонный метод
+    void templateMethod() {
+        step1();
+        step2();
+        step3();
+    }
+protected:
+    // Шаги алгоритма, которые подклассы должны реализовать
+    virtual void step1() = 0;
+    virtual void step2() = 0;
+    virtual void step3() = 0;
+};
+
+// Конкретный класс, реализующий шаги алгоритма
+class TemplateMethodClass : public AbstractClass {
+protected:
+    void step1() override {
+        std::cout << "Step 1" << std::endl;
+    }
+    void step2() override {
+        std::cout << "Step 2" << std::endl;
+    }
+    void step3() override {
+        std::cout << "Step 3" << std::endl;
+    }
+};
+
+int main() {
+    TemplateMethodClass templateMethod;
+    templateMethod.templateMethod();
+    return 0;
+}
